2a11e2e56ac95fb69baae89bbb9264c32c274d66
jinsiri <developer_corn@daum.net>
2025-05-15 15:14:44 +0900

    Feat: 파비콘 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,25 @@ import '@/styles/globals.css';
 export const metadata: Metadata = {
   title: 'TRUTH WORLD',
   description: '환영합니다!',
+  manifest: '/favicon/site.webmanifest',
+  icons: {
+    icon: [
+      { url: `${process.env.NEXT_PUBLIC_BASE_PATH || ''}/favicon/favicon.ico`, type: 'image/x-icon' },
+      { url: `${process.env.NEXT_PUBLIC_BASE_PATH || ''}/favicon/favicon-32x32.png`, sizes: '32x32', type: 'image/png' },
+      { url: `${process.env.NEXT_PUBLIC_BASE_PATH || ''}/favicon/favicon-16x16.png`, sizes: '16x16', type: 'image/png' },
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_PATH || ''}/favicon/android-chrome-192x192.png`,
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_PATH || ''}/favicon/android-chrome-512x512.png`,
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+    apple: [{ url: `${process.env.NEXT_PUBLIC_BASE_PATH || ''}/favicon/apple-touch-icon.png`, sizes: '180x180' }],
+  },
 };
 
 export default function RootLayout({
