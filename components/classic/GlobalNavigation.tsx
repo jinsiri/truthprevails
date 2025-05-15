9b8bd87330641db39b029b8906a4b8e9eb220380
jinsiri <developer_corn@daum.net>
2025-05-15 17:14:12 +0900

    Refactor: gnb 코드 정리, 활성화 페이지 링크 하이라이트 추가

--- components/classic/GlobalNavigation.tsx
+++ components/classic/GlobalNavigation.tsx
@@ -1,28 +1,34 @@
+'use client';
+
 import Link from 'next/link';
 import ThemeToggleButton from '@/components/ThemeToggleButton';
+import { usePathname } from 'next/navigation';
 
 export function GlobalNavigation() {
+  const pathname = usePathname();
+  const navItems = [
+    { href: '/classic', label: 'Home' },
+    { href: '/classic/about', label: 'About' },
+    { href: '/classic/career', label: 'Career' },
+    { href: '/classic/skills', label: 'Skills' },
+    { href: '/classic/education', label: 'Education' },
+    { href: '/classic/contact', label: 'Contact' },
+  ];
+
   return (
-    <nav className={'fixed top-5 left-1/2 flex w-full -translate-x-1/2 justify-between px-8 text-xl'}>
+    <nav className={'fixed top-5 left-1/2 z-50 flex w-full -translate-x-1/2 justify-between px-8 font-sans text-xl'}>
       <ul className={'flex gap-8'}>
-        <li>
-          <Link href={'/classic'}>Home</Link>
-        </li>
-        <li>
-          <Link href={'/classic/about'}>About</Link>
-        </li>
-        <li>
-          <Link href={'/classic/career'}>Career</Link>
-        </li>
-        <li>
-          <Link href={'/classic/skills'}>Skills</Link>
-        </li>
-        <li>
-          <Link href={'/classic/education'}>Education</Link>
-        </li>
-        <li>
-          <Link href={'/classic/contact'}>Contact</Link>
-        </li>
+        {navItems.map((item) => {
+          const isActive = pathname === item.href;
+
+          return (
+            <li key={item.href}>
+              <Link href={item.href} className={`transition-color duration-300 ${isActive ? 'text-amber-900' : 'text-black hover:text-amber-900'}`}>
+                {item.label}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
       <ThemeToggleButton />
     </nav>
