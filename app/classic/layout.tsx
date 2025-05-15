16109406efe411f7eb911caa40ff8f2632d5f3ee
jinsiri <developer_corn@daum.net>
2025-05-15 15:34:50 +0900

    Feat: 페이지 전환 모션 추가

--- app/classic/layout.tsx
+++ app/classic/layout.tsx
@@ -1,14 +1,31 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import { GlobalNavigation } from '@/components/classic/GlobalNavigation';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function ClassicLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
   return (
-    <main className={'classic w-full font-sans'}>
-      <GlobalNavigation />
-      {children}
-    </main>
+    <AnimatePresence mode='wait'>
+      <motion.div
+        key={pathname}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.2, ease: 'easeInOut' }}
+        style={{ willChange: 'opacity' }}
+      >
+        <main className={'classic w-full font-sans'}>
+          <GlobalNavigation />
+          {children}
+        </main>
+      </motion.div>
+    </AnimatePresence>
   );
 }
