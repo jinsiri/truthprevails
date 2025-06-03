24b67a1800666a5d87d6e8e30dd5814dcc9c8b65
jinsiri <developer_corn@daum.net>
2025-06-03 14:08:18 +0900

    UI: 클래식버전-contact 페이지 요소 크기 조정2

--- app/classic/contact/page.tsx
+++ app/classic/contact/page.tsx
@@ -6,9 +6,11 @@ import ThemedImage from '@/components/ThemedImage';
 
 export default function ClassicContact() {
   return (
-    <section className={'relative min-h-screen bg-purple-200 px-6 lg:px-0 xl:h-screen xl:overflow-hidden'}>
+    <section className={'relative min-h-screen bg-purple-200 px-6 pb-[50vw] md:pb-0 lg:px-0 xl:h-screen xl:overflow-hidden'}>
       <h2 className={'text-center text-2xl font-black uppercase md:text-5xl lg:text-7xl'}>Contact</h2>
-      <div className={'my-10 items-center text-base font-medium md:my-20 md:text-2xl lg:flex lg:h-3/4 lg:px-[25%] lg:text-3xl xl:my-0 xl:text-4xl'}>
+      <div
+        className={'my-10 items-center px-6 text-base font-medium md:my-20 md:px-10 md:text-2xl lg:flex lg:h-3/4 lg:text-3xl xl:my-0 xl:px-[25%] xl:text-4xl'}
+      >
         <ul className={'w-full space-y-2 rounded-xl border-4 bg-gray-50/50 p-4 md:space-y-6 md:p-10'}>
           <li className={'items-center gap-1 md:flex md:gap-2'}>
             <h3 className={'w-40 text-purple-700'}>E-MAIL</h3>
@@ -23,7 +25,7 @@ export default function ClassicContact() {
         </ul>
       </div>
       <motion.div
-        className={'-bottom-20 -left-40 max-w-1/2 lg:max-w-1/3 xl:absolute xl:-bottom-40'}
+        className={'absolute bottom-0 left-0 max-w-1/2 md:static lg:-bottom-20 lg:-left-40 lg:max-w-1/3 xl:absolute xl:-bottom-40'}
         initial={{ opacity: 0, x: -20, y: 20 }}
         animate={{ opacity: 1, x: 0, y: 0 }}
         transition={{ duration: 0.5 }}
