5de45c7d242708dc7b56ccbb67e9b5b6d3412a55
jinsiri <developer_corn@daum.net>
2025-06-19 22:41:23 +0900

    Fix: 토글 수행 후 포커스 해제

--- components/ThemeToggleButton.tsx
+++ components/ThemeToggleButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { MouseEvent, useEffect, useState } from 'react';
 import { IconMoonStars, IconSun } from '@tabler/icons-react';
 
 export default function ThemeToggleButton() {
@@ -16,18 +16,20 @@ export default function ThemeToggleButton() {
     document.documentElement.classList.toggle('dark', darkMode);
   }, []);
 
-  const toggleTheme = () => {
+  const toggleTheme = (e: MouseEvent<HTMLButtonElement>) => {
     const newTheme = isDark ? 'light' : 'dark';
     localStorage.setItem('theme', newTheme);
 
     document.documentElement.dataset.theme = newTheme;
     document.documentElement.classList.toggle('dark', newTheme === 'dark');
+    e.currentTarget.blur();
+
     setIsDark(!isDark);
   };
 
   return (
     <button
-      onClick={toggleTheme}
+      onClick={(e) => toggleTheme(e)}
       className='text-background hover:bg-elevated fixed top-[24] right-6 z-50 inline-flex cursor-pointer items-center justify-center rounded-full bg-amber-200 p-2 transition lg:top-[14] dark:bg-amber-500'
       aria-label='Toggle dark mode'
     >
