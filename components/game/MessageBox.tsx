9721458aa91e85f333926e02ee7c533611f062f9
jinsiri <developer_corn@daum.net>
2025-06-26 16:30:13 +0900

    Feat: 게임버전-알림창 컴포넌트화

---
+++ components/game/MessageBox.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { ReactNode } from 'react';
+
+interface MessageBoxProps {
+  onClose?: () => void;
+  children?: ReactNode;
+}
+
+export default function MessageBox({ onClose, children }: MessageBoxProps) {
+  return (
+    <div className='text-md fixed top-1/4 left-1/2 z-100 w-[90%] max-w-3xl -translate-x-1/2 rounded-xl border-4 bg-black/80 px-6 py-6 text-center text-white lg:text-2xl'>
+      <p>
+        <strong className={'mb-4 block border-t-2 border-b-2 py-2'}>* 알림 *</strong>
+        {children}
+      </p>
+      {onClose && (
+        <button onClick={onClose} className='mt-6'>
+          확인
+        </button>
+      )}
+    </div>
+  );
+}
