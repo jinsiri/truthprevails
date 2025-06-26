e99bec9542ede994282ac286ad8227fd381832ae
jinsiri <developer_corn@daum.net>
2025-06-26 17:19:38 +0900

    UI: 게임버전-알림창 y축 위치 조정

--- components/game/MessageBox.tsx
+++ components/game/MessageBox.tsx
@@ -9,8 +9,8 @@ interface MessageBoxProps {
 
 export default function MessageBox({ onClose, children }: MessageBoxProps) {
   return (
-    <div className='text-md fixed top-1/4 left-1/2 z-100 w-[90%] max-w-3xl -translate-x-1/2 rounded-xl border-4 bg-black/80 px-6 py-6 text-center text-white lg:text-2xl'>
-      <p>
+    <div className='text-md fixed top-1/2 left-1/2 z-100 w-[90%] max-w-3xl -translate-x-1/2 -translate-y-1/2 rounded-xl border-4 bg-black/80 px-6 py-6 text-center text-white lg:text-2xl'>
+      <p className={'break-keep'}>
         <strong className={'mb-4 block border-t-2 border-b-2 py-2'}>* 알림 *</strong>
         {children}
       </p>
