43de70b192e56c09940774a53a36b71767c5f5dd
jinsiri <developer_corn@daum.net>
2025-07-08 21:07:11 +0900

    Fix: 게임버전-Next SSR 페이지 동적 렌더링 명시

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,8 +1,10 @@
+export const dynamic = 'force-dynamic';
+
 import { headers } from 'next/headers';
 import { userAgent } from 'next/server';
 import GameMainClient from './GameMainClient';
 
-export default async function Page() {
+export default async function GameMain() {
   const headersList = await headers();
   const ua = userAgent({ headers: headersList });
 
