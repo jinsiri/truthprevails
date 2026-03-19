025db4d85ebaad16ea6b710dd705f6477bfb5c45
jinsiri <developer_corn@daum.net>
2026-03-19 22:46:51 +0900

    Feat: 로딩스피너 추가

--- app/game/GameMainClient.tsx
+++ app/game/GameMainClient.tsx
@@ -10,6 +10,7 @@ import MessageBox from '@/components/game/MessageBox';
 import Cloud from '@/components/game/Cloud';
 import Heart from '@/components/game/Heart';
 import Guide from '@/components/game/Guide';
+import clsx from 'clsx';
 
 type Direction = 'up' | 'down' | 'left' | 'right';
 const keyToDirection: Record<string, Direction> = {
@@ -75,16 +76,8 @@ export default function GameMainClient() {
     return !isMobile;
   });
   const [frame, setFrame] = useState(0);
-  const [position, setPosition] = useState(() => {
-    if (typeof window !== 'undefined') {
-      const saved = sessionStorage.getItem('lastPosition');
-      if (saved) {
-        const { x } = JSON.parse(saved);
-        return x;
-      }
-    }
-    return 0;
-  });
+  const [isReady, setIsReady] = useState(false);
+  const [position, setPosition] = useState(0);
 
   const [positionY, setPositionY] = useState(0);
   const [direction, setDirection] = useState<Direction | null>(null);
@@ -108,6 +101,13 @@ export default function GameMainClient() {
     : {
         transition: 'all 0.1s linear',
       };
+
+  useEffect(() => {
+    const savedPos = sessionStorage.getItem('lastPosition');
+    if (savedPos) {
+      setPosition(JSON.parse(savedPos).x);
+    }
+  }, []);
 
   useEffect(() => {
     const handleContextMenu = (e: MouseEvent) => e.preventDefault();
@@ -243,8 +243,13 @@ export default function GameMainClient() {
   useEffect(() => {
     if (isFirstRender.current) {
       isFirstRender.current = false;
+
+      setTimeout(() => {
+        setIsReady(true);
+      }, 500);
       return;
     }
+
     sessionStorage.setItem('guideState', JSON.stringify(guideState));
   }, [guideState]);
 
@@ -255,9 +260,34 @@ export default function GameMainClient() {
     return () => window.removeEventListener('keydown', handleKeyDown);
   }, [guideState, isMobile]);
 
+  if (!isReady)
+    return (
+      <div className='loading fixed z-[100] flex h-screen w-full items-center justify-center bg-blue-200'>
+        <div role='status'>
+          <svg
+            aria-hidden='true'
+            className='text-neutral-tertiary h-8 w-8 animate-spin fill-blue-600'
+            viewBox='0 0 100 101'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+              fill='currentColor'
+            />
+            <path
+              d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+              fill='currentFill'
+            />
+          </svg>
+          <span className='sr-only'>Loading...</span>
+        </div>
+      </div>
+    );
+
   return (
     <>
-      <section className='home group relative flex h-screen items-center justify-center overflow-hidden bg-sky-200 select-none'>
+      <section className={clsx('home group relative flex h-screen items-center justify-center overflow-hidden bg-sky-200 select-none')}>
         <Cloud />
         <Heart />
 
