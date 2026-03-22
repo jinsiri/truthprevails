1cf748dbc42e556a6fc4058d370b0e8f9c198bce
jinsiri <developer_corn@daum.net>
2026-03-22 22:34:53 +0900

    Feat: 게임버전-경력 내용 업데이트

--- app/classic/career/page.tsx
+++ app/classic/career/page.tsx
@@ -3,73 +3,7 @@
 import { motion } from 'framer-motion';
 import { Fragment, useState } from 'react';
 import ThemedImage from '@/components/ThemedImage';
-
-const tabTitles = ['SysOne (시스원)', '프리랜서', 'Mir9'];
-const experiences = [
-  {
-    title: 'ITSM 솔루션 개발',
-    data: [
-      { label: '기간', value: '2022.07 ~ 2024.11(2년 5개월)' },
-      { label: '프로젝트명', value: 'ITSM 솔루션 개발 및 유지보수' },
-      {
-        label: '성과',
-        value: [
-          '고객 요구사항 90% 이상 충족',
-          '시스템 운영 및 고객사 대응 프로세스 정립',
-          '신규 기능 개발 및 유지보수 처리 속도 30% 향상',
-          '구두 처리 업무 문서화',
-        ],
-      },
-      {
-        label: '역할',
-        value: [
-          '기능 개발 - ITSM 솔루션 신규 기능 개발 및 유지보수',
-          '환경 구성 - Docker 활용 개발 환경 설정',
-          '배포 및 유지보수 - 시스템 운영 및 장애 대응',
-          '사용자 지원 - 고객사 대상 사용법 안내 및 요구사항 조율',
-        ],
-      },
-      { label: '기술', value: 'React, Java, Spring Boot, JPA, Docker' },
-    ],
-  },
-  {
-    title: '외주 작업 / 웹 퍼블리싱',
-    data: [
-      { label: '기간', value: '2021.08 ~ 2022.06(11개월)' },
-      { label: '프로젝트명', value: '기업 웹사이트 구축/이벤트 페이지 작업' },
-      {
-        label: '성과',
-        value: ['클라이언트 요구사항 100% 반영', '프로젝트 일정 준수 및 품질 유지', '재계약율 80% 이상 유지'],
-      },
-      {
-        label: '역할',
-        value: ['웹 퍼블리싱 - 반응형 웹페이지 및 UI 구현', '고객 커뮤니케이션 – 요구사항 정리 및 피드백 반영', '추가 개발 - 간단한 기능 개발 및 유지보수'],
-      },
-      { label: '기술', value: 'HTML, CSS, JavaScript, jQuery, PHP' },
-    ],
-  },
-  {
-    title: '웹 에이전시 / 웹 퍼블리싱',
-    data: [
-      { label: '기간', value: '2018.07 ~ 2020.11(2년 5개월)' },
-      { label: '프로젝트명', value: '기업/공공기관 웹사이트 구축' },
-      {
-        label: '성과',
-        value: ['프로젝트 납기 준수율 100%', '접근성 및 반응형 최적화로 웹사이트 품질 향상', '기획-개발 간 커뮤니케이션 효율 증대 기여'],
-      },
-      {
-        label: '역할',
-        value: [
-          '웹 퍼블리싱 - HTML, CSS, JS 활용한 화면 디자인 구현',
-          '디자인 구현 - 기획/디자인 시안 반영 및 인터랙션 추가',
-          '협업 조율 - 기획자-백엔드 개발자와 기능 연계 논의',
-          '웹 표준/접근성 준수 - 최적화된 코드 작성',
-        ],
-      },
-      { label: '기술', value: 'HTML, CSS, JavaScript, jQuery' },
-    ],
-  },
-];
+import { EXPERIENCE, EXPERIENCE_TABS } from '@/constants/dataset';
 
 export default function ClassicCareer() {
   const [activeTab, setActiveTab] = useState(0);
@@ -85,7 +19,7 @@ export default function ClassicCareer() {
       >
         <div className='relative z-10 w-full border-b border-gray-600 text-center text-sm font-medium text-gray-500 md:text-lg xl:text-lg dark:border-gray-700'>
           <ul className='-mb-px flex w-full flex-wrap'>
-            {tabTitles.map((title, index) => (
+            {EXPERIENCE_TABS.map((title, index) => (
               <li className='me-2' key={index}>
                 <button
                   onClick={() => setActiveTab(index)}
@@ -103,7 +37,7 @@ export default function ClassicCareer() {
         </div>
 
         <ul className='pt-4 pr-4 pl-4 text-left text-lg lg:pr-0'>
-          {experiences.map(
+          {EXPERIENCE.map(
             (exp, index) =>
               activeTab === index && (
                 <li key={index} className='mb-8'>
--- app/game/career/page.tsx
+++ app/game/career/page.tsx
@@ -3,8 +3,9 @@
 import ThemedImage from '@/components/ThemedImage';
 import SpeechBubble from '@/components/game/SpeechBubble';
 import clsx from 'clsx';
-import { useEffect, useRef } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
 import { useKeyboardList } from '@/hooks/useKeyboardList';
+import { EXPERIENCE, EXPERIENCE_TABS } from '@/constants/dataset';
 
 interface Mode {
   companyNm: string;
@@ -14,9 +15,12 @@ const MODES: Mode[] = [{ companyNm: '시스원' }, { companyNm: '프리랜서' }
 
 export default function GameCareer() {
   const containerRef = useRef<HTMLUListElement>(null);
+  const [active, setActive] = useState(false);
   const { selectedIndex, handleKeyDown, setSelectedIndex } = useKeyboardList({
     items: MODES,
-    onSelect: () => {},
+    onSelect: () => {
+      setActive(true);
+    },
   });
 
   useEffect(() => {
@@ -35,37 +39,82 @@ export default function GameCareer() {
       />
 
       <section className='relative z-10 min-h-screen w-full p-8'>
-        <div className='group absolute right-1/4 -bottom-15 z-10'>
+        <div className={clsx('group absolute -bottom-15 z-10 transition-all duration-1000', active ? 'right-0' : 'right-1/4')}>
           <ThemedImage
             lightSrc={`/images/game/company_jinsil_day.png`}
             darkSrc={`/images/game/company_jinsil_night.png`}
-            width={300}
-            height={800}
+            width={active ? 400 : 300}
+            height={active ? 900 : 800}
             alt={'jinsil'}
           />
           <SpeechBubble text={'이런 일을 했습니다!'} />
         </div>
 
-        <div className={'absolute bottom-30 left-1/4 w-[46%]'}>
-          <div className='block h-[380px] w-full rounded-lg border-8 border-gray-400 bg-white p-4 md:p-8'>
-            <h2 className={'mb-4 text-2xl md:mb-6 md:text-4xl'}>경력사항</h2>
-            <ul
-              ref={containerRef}
-              onKeyDown={handleKeyDown}
-              tabIndex={0}
-              className={'mode-btns flex max-w-[70%] cursor-pointer flex-col gap-x-6 gap-y-2 text-xl outline-none md:text-2xl'}
-            >
-              {MODES.map((mode, index) => (
-                <li
-                  key={mode.companyNm}
-                  onMouseEnter={() => setSelectedIndex(index)}
-                  className={clsx('flex items-center justify-between px-4 py-2 hover:text-blue-900', selectedIndex === index && 'active bg-gray-200')}
+        <div className={clsx('absolute left-1/4 h-[50vh] w-[46%] transition-all duration-1000', active ? 'bottom-5 left-[5%] h-[90vh] w-[90%]' : 'bottom-20')}>
+          <div className='block h-3/4 w-full overflow-y-auto rounded-lg border-8 border-gray-400 bg-white p-4 md:p-8'>
+            {!active && (
+              <>
+                <h2 className={'mb-4 text-2xl md:mb-6 md:text-4xl'}>경력사항</h2>
+                <ul
+                  ref={containerRef}
+                  onKeyDown={handleKeyDown}
+                  tabIndex={0}
+                  className={clsx(
+                    'mode-btns flex w-full max-w-[70%] cursor-pointer flex-col gap-x-6 gap-y-2 text-xl outline-none md:text-2xl',
+                    active && 'max-w-full',
+                  )}
                 >
-                  {mode.companyNm}
-                  {selectedIndex === index && <span>Enter</span>}
-                </li>
-              ))}
-            </ul>
+                  {EXPERIENCE_TABS.map((mode, index) => (
+                    <li
+                      key={mode}
+                      onMouseEnter={() => setSelectedIndex(index)}
+                      onClick={() => setActive(true)}
+                      className={clsx('flex items-center justify-between px-4 py-2 hover:text-blue-900', selectedIndex === index && 'active bg-gray-200')}
+                    >
+                      {mode}
+                      {selectedIndex === index && <span>Enter</span>}
+                    </li>
+                  ))}
+                </ul>
+              </>
+            )}
+
+            {active && (
+              <>
+                <h3 className={'mb-4 text-2xl md:mb-6 md:text-4xl'}>{EXPERIENCE_TABS[selectedIndex]}</h3>
+                <ul className='pt-2 text-left text-lg lg:pr-0'>
+                  {EXPERIENCE.map(
+                    (exp, index) =>
+                      selectedIndex === index && (
+                        <li key={index} className='mb-8'>
+                          <h3 className='mb-4 text-xl font-bold text-gray-700'>{exp.title}</h3>
+
+                          <div className='w-full border-collapse text-left text-sm text-gray-700 lg:block rtl:text-right'>
+                            <div className='border-t border-gray-700 bg-gray-50/50 md:grid md:grid-cols-[150px_1fr]'>
+                              {exp.data.map((item, idx) => (
+                                <Fragment key={idx}>
+                                  <div className='border-gray-700 bg-gray-100 p-3 font-semibold md:border-b'>{item.label}</div>
+                                  <div className='border-b border-gray-700 p-3'>
+                                    {Array.isArray(item.value) ? (
+                                      <ul className='list-inside list-disc space-y-1'>
+                                        {item.value.map((v, i) => (
+                                          <li key={i}>{v}</li>
+                                        ))}
+                                      </ul>
+                                    ) : (
+                                      item.value
+                                    )}
+                                  </div>
+                                </Fragment>
+                              ))}
+                            </div>
+                          </div>
+                        </li>
+                      ),
+                  )}
+                </ul>
+              </>
+            )}
           </div>
           <canvas className={'absolute left-[5%] block h-full w-[30px] border-l-6 border-gray-800 bg-gradient-to-r from-gray-800 to-gray-400'} />
           <canvas className={'absolute right-[5%] block h-full w-[30px] border-l-6 border-gray-800 bg-gradient-to-r from-gray-800 to-gray-400'} />
