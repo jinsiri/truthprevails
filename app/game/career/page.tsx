e6968f3511331e93f391a31d98c795c79c739ead
jinsiri <developer_corn@daum.net>
2026-03-23 22:03:25 +0900

    Refactor: 게임버전-경력 모바일 UI 추가

--- app/game/career/page.tsx
+++ app/game/career/page.tsx
@@ -49,23 +49,22 @@ export default function GameCareer() {
       />
 
       <section className='relative z-10 min-h-screen w-full p-8'>
-        <div className={clsx('group absolute -bottom-15 z-10 hidden transition-all duration-1000 md:block', activeView === 'career' ? 'right-0' : 'right-1/4')}>
-          <ThemedImage
-            lightSrc={`/images/game/company_jinsil_day.png`}
-            darkSrc={`/images/game/company_jinsil_night.png`}
-            width={activeView === 'career' ? 400 : 300}
-            height={activeView === 'career' ? 900 : 800}
-            alt={'발표하는 캐릭터'}
-          />
-          <SpeechBubble text={'이런 일을 했습니다!'} />
-        </div>
-
         <div
           className={clsx(
             'absolute bottom-0 left-1/2 w-[90%] -translate-x-1/2 transition-all duration-1000',
             activeView === 'career' ? 'h-[90vh]' : 'h-[70vh] max-w-[1000px]',
           )}
         >
+          <div
+            className={clsx(
+              'group absolute -bottom-15 z-10 hidden aspect-[651/950] w-[30%] transition-all duration-1000 md:block',
+              activeView === 'career' ? 'right-10 max-w-[400px]' : '-right-20',
+            )}
+          >
+            <ThemedImage lightSrc={`/images/game/company_jinsil_day.png`} darkSrc={`/images/game/company_jinsil_night.png`} fill alt={'발표하는 캐릭터'} />
+            <SpeechBubble text={'이런 일을 했습니다!'} />
+          </div>
+
           <div className='block h-3/4 w-full overflow-y-auto rounded-lg border-8 border-gray-400 bg-gray-50 p-4 md:p-8 lg:p-12'>
             {activeView !== 'career' && (
               <>
@@ -74,7 +73,7 @@ export default function GameCareer() {
                   ref={containerRef}
                   onKeyDown={handleKeyDown}
                   tabIndex={0}
-                  className={clsx('mode-btns flex w-full max-w-[70%] cursor-pointer flex-col gap-x-6 gap-y-2 text-xl outline-none md:text-2xl')}
+                  className={clsx('mode-btns flex cursor-pointer flex-col gap-x-6 gap-y-2 text-xl outline-none md:w-[80%] md:text-2xl')}
                 >
                   {EXPERIENCE_TABS.map((mode, index) => (
                     <li
@@ -93,7 +92,7 @@ export default function GameCareer() {
 
             {activeView === 'career' && (
               <>
-                <div className={'mb-4 flex items-center justify-between gap-x-4 md:mb-6'}>
+                <div className={'mb-4 flex items-center justify-between gap-x-4 md:mb-6 md:w-[80%]'}>
                   <h3 className={'text-2xl md:text-4xl'}>{EXPERIENCE_TABS[hIdx]}</h3>
                   <div ref={careerDetailRef} className={'flex items-center outline-none'} onKeyDown={handleKeyDown} tabIndex={0}>
                     <button
@@ -117,7 +116,7 @@ export default function GameCareer() {
                   </div>
                 </div>
 
-                <ul className='w-[90%] pt-2 text-left text-lg lg:pr-0'>
+                <ul className='pt-2 text-left text-lg md:w-[80%] lg:pr-0'>
                   {EXPERIENCE.map(
                     (exp, index) =>
                       hIdx === index && (
